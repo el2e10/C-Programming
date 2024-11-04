2557543c1dfb38880a21fca6c932a415d547aed2
el2e10 <eldhoittangeorge@gmail.com>
2024-11-04 21:57:56 +0530

    Add 3.6
    Program to add padding to itoa()

---
+++ Chapter 3/ex-3.6.c
@@ -0,0 +1,72 @@
+#include <float.h>
+#include <limits.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_array(char arr[]) {
+
+  for (int i = 0; arr[i] != '\0'; i++) {
+    printf("%c", arr[i]);
+  }
+  printf("\n");
+}
+
+void reverse(char arr[]) {
+  int arr_len = 0;
+  for (; arr[arr_len] != '\0'; arr_len++)
+    ;
+
+  char tmp;
+  for (int i = 0; i <= arr_len / 2; i++) {
+    tmp = arr[i];
+    arr[i] = arr[arr_len - i - 1];
+    arr[arr_len - i - 1] = tmp;
+  }
+  arr[arr_len] = '\0';
+}
+
+void itoa(long n, char s[], int min_width) {
+
+  long sign;
+  int i = 0, correction = 0;
+
+  if ((sign = n) == LONG_MIN) {
+    correction = 1;
+    n = (n + 1) * -1;
+  } else if ((sign = n) < 0) {
+    n *= -1;
+  }
+
+  do {
+    char l = n % 10 + '0';
+    s[i++] = l;
+    // printf("%c\n", l);
+  } while ((n /= 10) > 0);
+
+  if (sign < 0) {
+    s[i++] = '-';
+  }
+
+  printf("%d\n", i);
+  for (; i < min_width; i++)
+    s[i] = '0';
+
+  s[0] = s[0] + correction;
+
+  s[i] = '\0';
+}
+
+int main() {
+  char c[100], d;
+  long j = 1 * pow(2, 18);
+  int min_width = 10;
+  printf("Converting %ld to string: \n", j);
+
+  itoa(j, c, min_width);
+
+  reverse(c);
+  print_array(c);
+
+  return 0;
+}
