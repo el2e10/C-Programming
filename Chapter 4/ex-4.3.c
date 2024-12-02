c4d3daf11d514c99add8a6410e1c163e09610074
el2e10 <eldhoittangeorge@gmail.com>
2024-12-02 22:02:36 +0530

    Add 4.5, 4.6 and 4.7 to 4.3

--- Chapter 4/ex-4.3.c
+++ Chapter 4/ex-4.3.c
@@ -1,10 +1,15 @@
 #include <ctype.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define INPUT_SIZE 100
 #define NUMBER '0'
+#define SIN '1'
+#define EXP '2'
+#define POW '3'
+#define VAR '4'
 
 int sp = 0, input_index = 0;
 int stack[INPUT_SIZE];
@@ -29,7 +34,7 @@ void print_stack(void) {
 	int i;
 	printf("[");
 	for (i = 0; i < sp; i++) {
-		if (i == sp-1) {
+		if (i == sp - 1) {
 			printf("%d]\n", stack[i]);
 		} else {
 			printf("%d, ", stack[i]);
@@ -59,12 +64,28 @@ void clear_stack(void) {
 	}
 }
 
+void ungets(char input[], char new[]) {
+	int input_len = strlen(input), new_len = strlen(new), i;
+	if (INPUT_SIZE < input_len + new_len) {
+		return;
+	}
+
+	for (i = 0; i < new_len; i++) {
+		input[input_len + i] = new[i];
+	}
+
+	input[input_len + i] = '\0';
+}
+
 int main(void) {
-	char input[INPUT_SIZE] = "-10 2";
+	int valid_expression = 1;
+	char input[INPUT_SIZE] = "3 8 ";
 	char number[INPUT_SIZE];
 	char c;
 
-	while ((c = gettop(input)) != '\0') {
+	ungets(input, "9 3 - + +");
+
+	while ((c = gettop(input)) != '\0' && valid_expression) {
 		int result;
 		int op2, op1;
 		switch (c) {
@@ -105,11 +126,33 @@ int main(void) {
 			result = pop() % op2;
 			push(result);
 			break;
+		case SIN:
+			push(sin(pop()));
+			printf("sin function detected\n");
+			break;
+		case POW:
+			op2 = pop();
+			op1 = pop();
+			push(pow(op2, op1));
+			printf("pow function detected\n");
+			break;
+		case EXP:
+			push(exp(pop()));
+			printf("exp function detected\n");
+			break;
+		case VAR:
+			/*
+			 * We just replace the variable with the top element in stack
+			 * */
+			if (sp == 0) {
+				printf("Invalid expression\n");
+				valid_expression = 0;
+				break;
+			}
+			push(stack[sp - 1]);
+			break;
 		}
 	}
-	print_stack();
-	clear_stack();
-	print_stack();
 	printf("The final result of stack is %d\n", stack[0]);
 	return 0;
 }
@@ -130,6 +173,21 @@ char gettop(char s[]) {
 	} else if (s[input_index] == '-' && s[input_index + 1] != ' ' &&
 			   s[input_index + 1] != '\0') {
 		return NUMBER;
+	} else if (s[input_index] == 's' && s[input_index + 1] == 'i' &&
+			   s[input_index + 2] == 'n') {
+		input_index += 3;
+		return SIN;
+	} else if (s[input_index] == 'e' && s[input_index + 1] == 'x' &&
+			   s[input_index + 2] == 'p') {
+		input_index += 3;
+		return EXP;
+	} else if (s[input_index] == 'p' && s[input_index + 1] == 'o' &&
+			   s[input_index + 2] == 'w') {
+		input_index += 3;
+		return POW;
+	} else if (isalpha(s[input_index])) {
+		input_index++;
+		return VAR;
 	} else {
 		return s[input_index++];
 	}
